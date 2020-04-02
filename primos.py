456b880a14179b4d521f57b6cf3b8a976188318b
Anderson-Alves1994 <61031977+Anderson-Alves1994@users.noreply.github.com>
2020-04-02 19:56:22 -0300

    ac

--- primos.py
+++ primos.py
@@ -5,11 +5,29 @@ from math import sqrt
 app = Flask(__name__)
 
 @app.route('/')
-def nao_entre_em_panico():
 
-    primos = "Nao esquecam de responder a pergunta <br> para nao ficar com falta!"
+def primo (n):
+    if n != 0 and n !=1:
+        if n>3:
+            for i in range(2,n):
+                if n %i==0:
+                    return False
+        return True
+    return
 
-    return primos
+
+i=1
+cont =0
+while True:
+    primo(i)
+    i+=1
+    if primo(i) == True:
+        cont+=1
+        return primo
+    if cont == 100:
+        break
+
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
